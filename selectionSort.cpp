b4bcaf0575d419b4ed8ac6084609dd65bceb1b41
Cantsnn <48149373+Cantsnn@users.noreply.github.com>
2020-07-11 18:49:25 +0300

    Create selectionSort.cpp

---
+++ selectionSort.cpp
@@ -0,0 +1,40 @@
+//Selection Sort 
+//iterasyon ve karsilastirma sayilari ile birlikte göstermektedir.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Eleman sayisi :";
+    cin>>n;
+    int dizi[n];
+    for(int i =0;i<n;i++){
+        cout<<i+1<<". eleman :";
+        cin>>dizi[i];
+    }
+
+    int temp,count=1;;
+    for(int i =0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+                cout<<i+1<<". iterasyon ve "<<count++<<". karsilastirma : "<<dizi[i]<<" ile "<<dizi[j];
+            if(dizi[i]>dizi[j]){
+                temp = dizi[i];
+                dizi[i]=dizi[j];
+                dizi[j]=temp;
+                cout<<" <-->"; //yer degistirmeyi gosterir.
+            }
+            cout<<endl;
+        }
+    }
+
+    cout<<"Siralanmis dizi : ";
+    for(int i =0;i<n;i++){
+    cout<<dizi[i];
+        if(i!=n-1)
+            cout<<",";
+   }
+    return 0;
+}
